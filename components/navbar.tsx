04a2abd4679196af4eab9c771201d1eced3650ea
arifire21 <ariwolfpup@icloud.com>
2023-12-13 07:11:36 -0500

    home page progress

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,29 +1,67 @@
+import { useEffect, useState } from 'react';
 import { Dropdown } from '@mui/base/Dropdown';
 import { MenuButton } from '@mui/base/MenuButton';
 import { Menu } from '@mui/base/Menu';
 import { MenuItem } from '@mui/base/MenuItem';
 import { HiOutlineMenu, HiOutlineX } from 'react-icons/hi'
 import Link from "next/link";
-// import styles from '@/styles/navbar.styles.scss';
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
+
+import LogoSmall  from '@/public/logo_transparent_crop_white.png';
+
+// const dropdown = document.getElementsByClassName('dropdown-main')[0]
+
+// //test: observe for the Mui-expanded (dropdown) class
+// let observer = new MutationObserver(mutations =>
+//   {
+//     mutations.forEach(MutationRecord => {
+//       const target = MutationRecord.target as HTMLElement
+//       if(target.className === 'Mui-expanded'){
+//         setArrow('&#9650')
+//       } else {
+//         setArrow('&#9660;')
+//       }
+//     })
+//   }
+// );
+
+// observer.observe(dropdown, {
+//   attributes: true,
+//   attributeFilter: ['style', 'class']
+// })
 
 export default function Navbar() {
-  // export default function MenuSimple() {
-  //   // const createHandleMenuClick = (menuItem: string) => {
-  //   //   return () => {
-  //   //     console.log(`Clicked on ${menuItem}`);
-  //   //   };
-  //   // };
+  // const [arrow, setArrow] = useState<HTMLElement>(&#9660;) //down-facing carat
+  const pathname = usePathname()
+
+  //handlers instead?
+  // function 
 
   return (
-    <div className='container'>
-      {/* <Image/> */}
-            <Dropdown>
-        <MenuButton>Services</MenuButton>
-        <Menu>
-          <MenuItem><Link href="/services/parking">Parking & Monitoring</Link></MenuItem>
-          <MenuItem><Link href="/services/painting">Painting</Link></MenuItem>
+    <nav className='nav-container'>
+      <Link href="/" title='Home'>
+        <Image src={LogoSmall} height={100} width={100} alt='eagle-eye-home' style={{paddingLeft:'1rem'}}/>
+      </Link>
+      <Dropdown>
+        <MenuButton className='dropdown-main primary'>Services &#9660;</MenuButton>
+        <Menu className='dropdown-menu'>
+          <MenuItem className='dropdown-item'>
+            <Link className={`${pathname === '/services/parking' ? 'active' : ''}`}
+                  href="/services/parking" title='Parking & Monitoring'>Parking & Monitoring</Link>
+          </MenuItem>
+          <MenuItem className='dropdown-item'>
+            <Link className={`${pathname === '/services/painting' ? 'active' : ''}`}
+                  href="/services/painting" title='Painting'>Painting</Link>
+          </MenuItem>
         </Menu>
       </Dropdown>
-    </div>
+      <Link className={`nav-item ${pathname === '/about' ? 'active' : ''}`}
+            href="/about" title='About Us'>About Us
+      </Link>
+      <Link className={`nav-item ${pathname === '/contact' ? 'active' : ''}`}
+            href="/contact" title='Contact Us'>Contact Us
+      </Link>
+    </nav>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import styles from '@/styles/Home.module.scss';
 import CTAButton from '@/components/button';
 
 import Logo  from '@/public/logo_transparent.png';
+import Site1 from '../images/site1.png';
+import Site2 from '../images/site1.png';
 
 export default function Home() {
   return (
@@ -13,22 +15,60 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <meta></meta>
       </Head>
+    <div className='main-padding'>
+        <div className='mainContainer'>
+          <Image src={Logo} alt='Eagle Eye LLC.' width={400} height={400}/>
+          <h1 className={styles.sub}>Parking Enforcement and Monitoring Services</h1>
+          <p className={styles.tagline}>If you want happy tenants and customers tomorrow, you’ll hire us today!</p>
 
-      {/* <Navbar/> */}
-      <h1 className="">
-      Hello world!
-    </h1>
+          <div className='sbsContainer'>
+            <section className='sbsText'>
+              <p>If you have a situation with vehicles parking on your property where they should not be parking, then that is when our services come in and you allow Eagle Eye Monitoring, LLC. to work for you. We are a licensed and insured parking enforcement company. We specialize in helping property owners, managers, and homeowner associations with their parking issues 24 hours a day, 7 days a week.</p>
+              <p>Using our software, we provide streamlined online registration, virtual permitting, and record-keeping. Property managers and owners are able to access our programs at any time, and opt-in to mobile alerts, to stay informed about their properties’ security.</p>
 
-    <CTAButton variant='primary' text='test'/>
-    <CTAButton variant='secondary' text='other test'/>
+              <CTAButton variant='primary' text='Learn More'/>
 
-    <p>If you want happy tenants and customers tomorrow, you’ll hire us today!</p>
+              <p>Our goal is to ensure that your property is maintained in compliance with all local and state parking ordinances, as well as your parking rules. We take the hassle of finding all the vehicles in violation out of your hands. We work hand and hand with various local tow truck companies. Eagle Eye will be there as your representative any time day or night.</p>
+            </section>
+            <Image src={Site1} alt='parking-lot' width={470} height={355}/>
+          </div>
 
-    <div className={styles.container}>
-    <Image src={Logo} alt='logo' className={styles.item} width={400} height={400}/>
-    <section className={styles.item}>
-    </section>
-    </div>
+          <section>
+            <h2>Our Specialties</h2>
+            <p>Single Family Homes <span className={styles.bullet}>&bull;</span> Apartment Communities <span className={styles.bullet}>&bull;</span> Gated Communities <span className={styles.bullet}>&bull;</span> COA’s <span className={styles.bullet}>&bull;</span> HOA’s <span className={styles.bullet}>&bull;</span> Commercial Properties</p>
+          </section>
+
+          <div className='sbsContainer'>
+              <Image src={Site2} alt='parking-lot-text' width={395} height={225}/>
+              <section className='sbsText'>
+                <h2>We Assist With:</h2>
+                <ol>
+                  <li>customizable 24 hour patrol for your property</li>
+                  <li>Handicap parking space and Accessible Violations</li>
+                  <li>Fire Lanes Violations</li>
+                  <li>No Parking Zones</li>
+                  <li>Tenant Parking Only</li>
+                  <li>Dumpster/ Entrance Blocked</li>
+                  <li>Taking Multiple Parking Spaces</li>
+                  <li>No Permit Parking/Reserved Space violations</li>
+                  <li>Parking on Landscaping or grass</li>
+                  <li>Repainting parking lines</li>
+                  and more!
+                </ol>
+
+                <div className='sbsContainer sbsItem'>
+                  <CTAButton variant='primary' text='Monitoring Services'/>
+                  <CTAButton variant='secondary' text='Painting Services'/>
+                </div>
+              </section>
+            </div>
+
+            <div className='sbsText sbsItem'>
+              <p>We know each property has different needs and requirements. Schedule a meeting with our manager--we will be happy to survey your property and discuss available options!</p>
+              <CTAButton variant='primary' text='Contact Us'/>
+            </div>
+        </div>
+      </div>
     </>
   );
 }

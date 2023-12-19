864cc1dd5fde91cad129854958fa8f5cdc113c0a
arifire21 <ariwolfpup@icloud.com>
2023-12-19 07:10:54 -0500

    fixed form styling

--- components/form.tsx
+++ components/form.tsx
@@ -13,36 +13,40 @@ export default function QuoteForm() {
     <form className={styles.form}>
       <FormControl defaultValue="" required>
         <label className={styles.label}>First and Last Name <sup>*</sup></label>
-        <Input className={`${styles.formInput} ${styles.textInput}`} aria-label="Full Name input" />
+        <Input className={styles.formInput} aria-label="Full Name input" />
       </FormControl>
 
       <FormControl defaultValue="" required>
         <label className={styles.label}>Email <sup>*</sup></label>
-        <Input className={`${styles.formInput} ${styles.textInput}`} aria-label="Email input" placeholder="example@domain.com" type='email'/>
+        <Input aria-label="Email input" placeholder="example@domain.com" type='email'/>
       </FormControl>
 
       <FormControl defaultValue="" required>
         <label className={styles.label}>Name of Complex or Business<sup>*</sup></label>
-        <Input className={`${styles.formInput} ${styles.textInput}`} aria-label="Name of Complex or Business input" />
+        <Input aria-label="Name of Complex or Business input" />
       </FormControl>
 
       <FormControl defaultValue="" required>
         <label className={styles.label}>Address of Complex or Business <sup>*</sup></label>
-        <Input className={`${styles.formInput} ${styles.textInput}`} aria-label="Address of Complex or Business input"/>
+        <Input aria-label="Address of Complex or Business input"/>
       </FormControl>
 
-    <FormControl defaultValue="" required style={{flexDirection:'row'}}>
       <label className={styles.label}>Service Requested <sup>*</sup></label>
-      <label htmlFor='monitor'>Monitoring/Parking</label>
-      <Input id='monitor' name='service-radio' aria-label="Monitoring/Enforcement radio button input" type='radio'/>
-      <label htmlFor='painting'>Painting</label>
-      <Input id='paint' name='service-radio' aria-label="Painting radio button input" type='radio'/>
-    </FormControl>
+      <FormControl defaultValue="" required className={styles.radioContainer}>
+        <div className={styles.radioItem}>
+          <Input id='monitor' name='service-radio' aria-label="Monitoring/Enforcement radio button input" type='radio'/>
+          <label htmlFor='monitor'>Monitoring/Parking</label>
+        </div>
+        <div className={styles.radioItem}>
+          <Input id='paint' name='service-radio' aria-label="Painting radio button input" type='radio'/>
+          <label htmlFor='paint'>Painting</label>
+        </div>
+      </FormControl>
 
-    <FormControl defaultValue="" required>
-      <label className={styles.label}>Additional Details <sup>*</sup></label>
-      <TextareaAutosize className={styles.formInput} aria-label="Details of service Textarea" minRows={3} />
-    </FormControl>
+      <FormControl defaultValue="" required>
+        <label className={styles.label}>Additional Details <sup>*</sup></label>
+        <TextareaAutosize className={styles.formInput} aria-label="Details of service Textarea" minRows={4} />
+      </FormControl>
     </form>
   )
 }
